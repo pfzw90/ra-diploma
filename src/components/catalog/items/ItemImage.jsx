660a40c1cfa069d8402254ec02540783531c58a1
Natalya Chekhonina <pfzw90@gmail.com>
2021-09-24 15:10:16 +0300

    test

---
+++ src/components/catalog/items/ItemImage.jsx
@@ -0,0 +1,38 @@
+/* eslint-disable no-loop-func */
+/* eslint-disable array-callback-return */
+import React, { useEffect, useState } from 'react';
+import Loader from '../../loader/Loader.jsx';
+
+const InitialState = { state: 'idle', img: null };
+
+const testImage = (url) => new Promise((resolve) => {
+  const img = new Image();
+  img.src = url;
+  img.onload = () => resolve(true);
+  img.onerror = () => resolve(false);
+});
+
+export default function ItemImage(props) {
+  console.log(props);
+  const [imageState, setImageState] = useState(InitialState);
+
+  useEffect(() => {
+    setImageState((state) => ({ ...state, state: 'loading' }));
+    let imgOk;
+    for (let i = 0; i < props.img.length; i += 1) {
+      if (!imgOk) {
+        testImage(props.img[i]).then((result) => {
+          imgOk = result;
+        });
+        if (imgOk) setImageState(({ img: props.img[i], state: 'idle' }));
+      }
+    }
+  },
+  [props]);
+
+  if (imageState.state === 'loading') return <Loader/>;
+  const img = imageState.img || process.env.REACT_APP_NO_IMAGE_URL;
+  return (
+    <img src={img} alt={props.title}/>
+  );
+}
